1e414626e1954df757214fb8dbcbf9bc1b4c4f9e
Rajesh <rajeshraj8558@gmail.com>
2024-12-10 10:20:51 +0530

    update

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.Path.dirname(os.path.abspath(__file__))).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#-9-5_%7t)*&yv8@7l)dm2raa0-1j0)+9dk)2x3*&&mo$t!s+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -131,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR,'static')
+  os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
