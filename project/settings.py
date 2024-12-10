6e2b352a81699babd192724515162051a70f13df
Rajesh <rajeshraj8558@gmail.com>
2024-12-10 08:51:30 +0530

    smch

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ import os
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR,'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles-build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
